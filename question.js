9ea5c3438e217a940542c6c14b37bb0f1a159e49
mdTarek-web <mdtarek2330749@gmail.com>
2025-04-26 19:38:17 +0600

    Add files via upload

---
+++ question.js
@@ -0,0 +1,185 @@
+let questions = [
+    {
+        numb:1,
+        question:"Which of the following is an example of a (social media platform)?",
+        answer: "Both B and C",
+        options:[
+             "Microsoft Word",
+            " Facebook",
+            " YouTube",
+            "Both B and C"
+        ]
+      },
+
+      {
+        numb: 2,
+        question:"What does ICT stand for?",
+        answer: "Information and Communication Technology",
+        options:[
+           " Internet and Computer Technology",
+           "Information and Communication Technology",
+           " Information Communication Transfer",
+          "Integrated Communication Technology"
+        ]
+      },
+
+      {
+        numb:3,
+        question:"Which of the following is NOT a type of computer hardware?",
+        answer:"Operating System",
+        options:[
+          "Motherboard",
+          " Printer",
+           "Operating System",
+           " CPU"
+        ]
+      },
+
+      {
+        numb:4,
+        question:"What is cloud computing?",
+        answer:"Storing and accessing data over the internet",
+        options:[
+              "type of computer virus",
+             "Storing and accessing data over the internet",
+             "A new type of hardware",
+             " A backup device"
+        ]
+      },
+
+      {
+        numb:5,
+        question:"Which of these is a common operating system?",
+        answer:" Microsoft Windows",
+        options:[
+           " Google Chrome",
+          " Microsoft Windows",
+           "Adobe Photoshop",
+          " Instagram"
+        ]
+      },
+
+      {
+        numb:6,
+        question:"What does HTTP stand for in a website URL?",
+        answer:"HyperText Transfer Protocol",
+        options:[
+            "HyperText Transfer Protocol",
+           " High Technology Transfer Protocol",
+           "Hyper Terminal Technology Protocol",
+            "HyperText Translation Process"
+        ]
+      },
+
+      {
+        numb:7,
+        question:"What Does HTML Stand For?",
+        answer:"Hyper Text Markup Language",
+        options:[
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "What Does CSS Stand For?",
+        answer: "Cascading Style Sheet",
+        options: [
+          "Common Style Sheet",
+          "Colorful Style Sheet",
+          "Computer Style Sheet",
+          "Cascading Style Sheet"
+        ]
+      },
+
+      {
+        numb: 9,
+        question: "What Does PHP Stand For?",
+        answer: "Hypertext Preprocessor",
+        options: [
+          "Hypertext Preprocessor",
+          "Hypertext Programming",
+          "Hypertext Preprogramming",
+          "Hometext Preprocessor"
+        ]
+      },
+    
+      {
+        numb: 10,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+          "Stylish Question Language",
+          "Stylesheet Query Language",
+          "Statement Question Language",
+          "Structured Query Language"
+        ]
+      },
+
+      {
+        numb: 11,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+      
+      {
+        numb:12,
+        question:"What is the main purpose of a firewall in a computer network?",
+        answer:"To prevent unauthorized access",
+        options:[
+            "To prevent unauthorized access",
+           " To increase internet speed",
+           "To connect computers to the internet",
+           "To store backup data"
+        ]
+      },
+
+      {
+        numb:13,
+        question:"Which of the following is NOT a feature of an Operating System?",
+        answer:"Antivirus Protection",
+        options:[
+            "File Management",
+            " User Interface",
+            "Antivirus Protection",
+           " Memory Management"
+        ]
+      },
+
+      {
+        numb:14,
+        question:"What is the Internet of Things (IoT)?",
+        answer:"A system where everyday objects are connected to the internet",
+        options:[
+             "digital device for communication",
+             "A system where everyday objects are connected to the internet",
+             "A new form of web browser",
+             "A type of social media app" 
+        ]
+      },
+
+      {
+        numb:15,
+        question:"What is cybersecurity?",
+        answer:"Protection of computer systems and networks from cyber attacks",
+        options:[
+            "A type of internet connection",
+            "Protection of computer systems and networks from cyber attacks",
+           "A new programming language",
+           " A backup system for files"
+        ]
+      },
+
+ ]
+
+
+ 
